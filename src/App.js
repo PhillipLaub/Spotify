415d9a5b3ded76e9f2ea3e658413f23f75d2f586
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2020-10-21 10:19:36 -0700

    getting user info

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-import Login from './Login';
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player';
+
+//constructor
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  //run code based on given condition
+  useEffect(() => {
+    //code...
+    const hash = getTokenFromUrl();
+    //secure url, hide access token
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      //get user profile json
+      spotify.getMe().then((user) => {
+        console.log("person", user);
+      });
+    }
+
+    console.log("I have a token>>", token);
+  }, []);
+
   return (
-    //BEM
-    <div className="app">
-    
-      <Login />
-    </div>
+    <div className="app">{token ? <Player /> : <Login />}</div>
   );
 }
 
