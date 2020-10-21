826159c1e0afaa14ee4f58b22c968a800bde0010
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2020-10-21 14:53:49 -0700

    footer almost complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import './Sidebar.css';
-import SidebarOption from './SidebarOption';
-import HomeIcon from '@material-ui/icons/Home';
-import SearchIcon from '@material-ui/icons/Search';
-import LibraryMusicIcon from '@material-ui/icons/LibraryMusic';
-
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { useDataLayerValue } from "./DataLayer";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <img 
-            className="sidebar__logo"
-            src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
+  const [{ playlists }, dispatch] = useDataLayerValue();
 
-            <SidebarOption Icon={HomeIcon} title="Home"/>
-            <SidebarOption Icon={SearchIcon} title="Search"/>
-            <SidebarOption Icon={LibraryMusicIcon} title="Your Library"/>
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
 
-            <br/>
-            <strong className="sidebar__title">PLAYLISTS</strong>
-            <hr/>
+      <SidebarOption Icon={HomeIcon} title="Home" />
+      <SidebarOption Icon={SearchIcon} title="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
 
-            <SidebarOption title='hip hop' />
-        </div>
-    )
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption title={playlist.name} />
+      ))}
+
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
